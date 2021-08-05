ddb3a39d40812a5dc426643ceecbb77d34894c26
jbpita <jbpita@espol.edu.ec>
2021-08-05 13:51:42 -0500

    corrigiendo detalles de CardPaper component

--- src/components/Landing/CardPaper.js
+++ src/components/Landing/CardPaper.js
@@ -12,22 +12,31 @@ import {
 
 const useStyle = makeStyles((theme) => ({
 	root: {
-		maxWidth: '80vw'
+		maxWidth: '80vw',
+		boxShadow:' 0px 4px 4px rgba(0, 0, 0, 0.25)',
+		borderRadius: '50px',
+		bottom: '300px',
+		padding: '0'
 	},
-	details: {
-		display: 'flex',
-		flexDirection: 'column',
-	},
+
 	cover: {
 		width: '100%',
-		height: '100%'
+		minHeight: '100%',
+		backgroundSize: 'cover'
 	},
-	controls: {
+	container:{
+		boxShadow:' 0px 4px 4px rgba(0, 0, 0, 0.25)'
+	},
+	descripcion:{
 		display: 'flex',
 		alignItems: 'center',
-		paddingLeft: theme.spacing(1),
-		paddingBottom: theme.spacing(1),
+		justifyContent : 'space-around'
 	},
+	content:{
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	}
 }));
 
 const CardPaper = ({
@@ -44,9 +53,9 @@ const CardPaper = ({
 
 	return (
 		<div>
-			<Card className={classes.root}>
-				<Grid container spacing={0}>
-					<Grid item xs={4}>
+			<Card className={classes.root} >
+				<Grid container spacing={0} >
+					<Grid item xs={2}>
 						<CardMedia
 							className={classes.cover}
 							title="imagen Paper"
@@ -54,32 +63,34 @@ const CardPaper = ({
 							component="img"
 						/>
 					</Grid>
-					<Grid item xs={4}>
+					<Grid item xs={7.2}>
 						<div >
-							<CardContent>
-								<Typography variant="h5" component="h5">
+							<CardContent className={classes.content}>
+								<Typography variant="" component="h1">
 									{titulo}
 								</Typography>
-								<Typography variant="body" component="h2">
-									{autor}
-								</Typography>
-								<Typography variant="body2" component="p">
-									{fecha}
-								</Typography>
-								<Typography variant="body2" component="p">
-									{AreaEstudio}
-								</Typography>
+								<div className={classes.descripcion}>
+									<Typography variant="subtitle1">
+										{autor}
+									</Typography>
+									<Typography variant="subtitle1">
+										{fecha}
+									</Typography>
+									<Typography variant="subtitle1">
+										{AreaEstudio}
+									</Typography>
+								</div>
 								<Typography variant="body2" component="p">
 									{descripcion}
+								</Typography>
+								<Typography variant="body2" component="p">
+									{tags}
 								</Typography>
 							</CardContent>
 						</div>
 					</Grid>
-					<Grid item xs={4}>
-						<CardContent className={classes.extras}>
-							<Typography variant="body2" component="p">
-								{tags}
-							</Typography>
+					<Grid item xs={1}>
+						<CardContent>
 							<Typography variant="body2" component="p">
 								{numEstrellas}
 							</Typography>
