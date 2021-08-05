d44d6459927bb86317d5695db4660fe5496b5345
jbpita <jbpita@espol.edu.ec>
2021-08-04 23:28:14 -0500

    merge

--- src/components/Landing/CardPaper.js
+++ src/components/Landing/CardPaper.js
@@ -6,18 +6,20 @@ import imagePaper from './paper.jpg';
 import {
 	CardActionArea,
 	CardContent,
-	CardMedia,
 	Typography,
 } from '@material-ui/core';
 
 const useStyle = makeStyles((theme) => ({
 	root: {
 		diplay: 'flex',
-		maxHeight: 1000,
 		minWidth: '70vW',
 		flexWrap: "nowrap" 
 	},
 	child:{
+		maxWidth: '25%'
+	},
+	child2:{
+		maxWidth: '10%'
 	}
 }));
 
@@ -37,53 +39,46 @@ const CardPaper = (
 
 	return (
 		<div > 
-		<Card className={classes.root}>
-			{/*<CardActionArea className={classes.root}>*/}
-				<img
-					src={imagePaper}
-					alt="Paper"
-					width="282px"
-					height="281px"
-					className={classes.child}
-				/>
-				
-				{/*<CardMedia
-					component="img"
-					alt="imagen Paper"
-					image="./paper.jpg"
-					title="imagen Paper"
-				/>*/}
-
-				<CardContent className={classes.child}>
-					<Typography variant="h5" component="h2">
-						{titulo}
-					</Typography>
-					<Typography variant="body" component="h2">
-						{autor}
-					</Typography>
-					<Typography variant="body2" component="p">
-						{AreaEstudio}
-					</Typography>
-					<Typography variant="body2" component="p">
-						{descripcion}
-					</Typography>
-					<Typography variant="body2" component="p">
-						{fecha}
-					</Typography>
-					<Typography variant="body2" component="p">
-						{tags}
-					</Typography>
-				</CardContent>
-				<CardContent className={classes.child}>
-					<Typography variant="body2" component="p">
-						{numEstrellas}
-					</Typography>
-					<Typography variant="body2" component="p">
-						{gitHub}
-					</Typography>
-				</CardContent>
-			{/*</CardActionArea>*/}
-		</Card>
+		<div className={classes.root}>
+				<div>
+					<img
+						src={imagePaper}
+						alt="Paper"
+						width="252px"
+						height="271px"
+					/>
+				</div>		
+				<CardActionArea>
+					<CardContent>
+						<Typography variant="h5" component="h2">
+							{titulo}
+						</Typography>
+						<Typography variant="body" component="h2">
+							{autor}
+						</Typography>
+						<Typography variant="body2" component="p">
+							{AreaEstudio}
+						</Typography>
+						<Typography variant="body2" component="p">
+							{descripcion}
+						</Typography>
+						<Typography variant="body2" component="p">
+							{fecha}
+						</Typography>
+						<Typography variant="body2" component="p">
+							{tags}
+						</Typography>
+					</CardContent>
+					<CardContent >
+						<Typography variant="body2" component="p">
+							{numEstrellas}
+						</Typography>
+						<Typography variant="body2" component="p">
+							{gitHub}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+		</div>
 		</div>
 	);
 };
